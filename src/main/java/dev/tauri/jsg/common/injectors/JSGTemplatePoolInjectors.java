3ebb67e8d2f18d42b9eabc687e73f921d6ef44ab
VojtechSin <vojtech.sin@peveko.cz>
2026-07-15 01:53:49 +0200

    Fixed RL deprecation

--- src/main/java/dev/tauri/jsg/common/injectors/JSGTemplatePoolInjectors.java
+++ src/main/java/dev/tauri/jsg/common/injectors/JSGTemplatePoolInjectors.java
@@ -1,38 +1,38 @@
 package dev.tauri.jsg.common.injectors;
 
 import dev.tauri.jsg.core.common.worldgen.TemplatePoolInjector;
-import net.minecraft.resources.ResourceLocation;
+import dev.tauri.jsg.core.mapping.JSGMapping;
 
 public class JSGTemplatePoolInjectors {
     public static void register() {
         TemplatePoolInjector.Builder
-                .forTargets(new ResourceLocation("minecraft:ancient_city/city_center"))
-                .add(new TemplatePoolInjector.PoolAddition(new ResourceLocation("jsg:ancient_city/city_center")))
+                .forTargets(JSGMapping.rl("minecraft:ancient_city/city_center"))
+                .add(new TemplatePoolInjector.PoolAddition(JSGMapping.rl("jsg:ancient_city/city_center")))
                 .submit();
 
         TemplatePoolInjector.Builder
-                .forTargets(new ResourceLocation("minecraft:village/desert/town_centers"))
-                .add(new TemplatePoolInjector.PoolAddition(new ResourceLocation("jsg:village/desert/town_centers")))
+                .forTargets(JSGMapping.rl("minecraft:village/desert/town_centers"))
+                .add(new TemplatePoolInjector.PoolAddition(JSGMapping.rl("jsg:village/desert/town_centers")))
                 .submit();
 
         TemplatePoolInjector.Builder
-                .forTargets(new ResourceLocation("minecraft:village/plains/town_centers"))
-                .add(new TemplatePoolInjector.PoolAddition(new ResourceLocation("jsg:village/plains/town_centers")))
+                .forTargets(JSGMapping.rl("minecraft:village/plains/town_centers"))
+                .add(new TemplatePoolInjector.PoolAddition(JSGMapping.rl("jsg:village/plains/town_centers")))
                 .submit();
 
         TemplatePoolInjector.Builder
-                .forTargets(new ResourceLocation("minecraft:village/savanna/town_centers"))
-                .add(new TemplatePoolInjector.PoolAddition(new ResourceLocation("jsg:village/savanna/town_centers")))
+                .forTargets(JSGMapping.rl("minecraft:village/savanna/town_centers"))
+                .add(new TemplatePoolInjector.PoolAddition(JSGMapping.rl("jsg:village/savanna/town_centers")))
                 .submit();
 
         TemplatePoolInjector.Builder
-                .forTargets(new ResourceLocation("minecraft:village/snow/town_centers"))
-                .add(new TemplatePoolInjector.PoolAddition(new ResourceLocation("jsg:village/snow/town_centers")))
+                .forTargets(JSGMapping.rl("minecraft:village/snow/town_centers"))
+                .add(new TemplatePoolInjector.PoolAddition(JSGMapping.rl("jsg:village/snow/town_centers")))
                 .submit();
 
         TemplatePoolInjector.Builder
-                .forTargets(new ResourceLocation("minecraft:village/taiga/town_centers"))
-                .add(new TemplatePoolInjector.PoolAddition(new ResourceLocation("jsg:village/taiga/town_centers")))
+                .forTargets(JSGMapping.rl("minecraft:village/taiga/town_centers"))
+                .add(new TemplatePoolInjector.PoolAddition(JSGMapping.rl("jsg:village/taiga/town_centers")))
                 .submit();
     }
 }
